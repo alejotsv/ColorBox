3d87d52ed89a64b3a232ac7457c9cf5bcb20e40e
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 19:52:50 -0500

    Add CustomPanel and Main classes

---
+++ src/com/java21days/Main.java
@@ -0,0 +1,10 @@
+package com.java21days;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Color color = new Color(0, 255, 255);
+        ColorBox colorBox = new ColorBox("My Color Box", color);
+    }
+}
