ab572c631c3cb97e16e7b8b2be530f321a5391d6
Alejandro Salas <asalas.wd@outlook.com>
2022-11-23 19:32:09 -0500

    Create basic constructor

--- src/com/java21days/ColorBox.java
+++ src/com/java21days/ColorBox.java
@@ -1,6 +1,17 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class ColorBox extends JFrame {
+    Dimension dim = new Dimension(300, 300);
+
+    ColorBox(String title, Color color){
+        super(title);
+        setSize(dim);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
 }
